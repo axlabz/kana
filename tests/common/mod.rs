80a83ad4c3f1824d13b51565375bfe741b05f282
axemac <74874171+ax-lab@users.noreply.github.com>
2021-10-12 18:24:33 -0300

    [WIP] implement parsing for char test data files
    WIP:
    - broken `test_chars.rs`
    - move docs from and cleanup `chars.rs`
    
    - refactor char data types and code
    - implement parsing for lists, ranges, and flags
    - change self-check to use `flags.in`
    
    Syntax changes:
    - change comment format from `#` to `//`
    - change `:=` separator to `+`
    
    Minor:
    - fix invalid character literals in `flags.in`

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -11,26 +11,55 @@ use lazy_static::lazy_static;
 use regex::Regex;
 
 /// Returns an iterator for the lines on a file.
-pub fn read_lines<P: AsRef<Path>>(filename: P) -> io::Result<impl Iterator<Item = String>> {
+pub fn read_lines<P: AsRef<Path>>(
+	filename: P,
+) -> io::Result<impl Iterator<Item = (String, usize)>> {
 	let file = File::open(filename)?;
 	Ok(BufReader::new(file)
 		.lines()
-		.map(|x| {
-			let line = x.unwrap();
+		.enumerate()
+		.map(|(pos, line)| {
+			let line = line.unwrap();
 			let line = line.trim();
-			let line = match line.find('#') {
+			let line = match line.find("//") {
 				Some(index) => line[..index].trim(),
 				None => line,
 			};
-			line.to_owned()
+			(line.to_owned(), pos + 1)
 		})
-		.filter(|x| x.len() > 0))
+		.filter(|(x, _)| x.len() > 0))
 }
 
+/// A line of data from a character test data file.
 pub struct TestChar {
-	pub char: char,
-	pub code: String,
-	pub description: String,
+	/// Source line number.
+	pub line: usize,
+
+	/// Data for the line.
+	pub info: TestCharInfo,
+}
+
+/// Information for a [`TestChar`].
+pub enum TestCharInfo {
+	/// Defines a single character.
+	Single {
+		char: char,
+		code: String,
+		description: String,
+	},
+
+	/// Defines a range of characters.
+	Range {
+		start: char,
+		end: char,
+		description: String,
+	},
+
+	/// Defines a list of characters.
+	List(Vec<char>),
+
+	/// Defines a list of flags for following characters.
+	Flags(kana::Flags),
 }
 
 /// Returns an iterator for character test files.
@@ -43,53 +72,148 @@ pub fn read_chars<P: AsRef<Path> + std::fmt::Display>(
 
 	let fileinfo = format!("{}", filename);
 	let lines = read_lines(filename)?;
-	let chars = lines.map(move |x| {
-		let mut iter = x.split('\t');
-		let f1 = match iter.next() {
-			Some(x) => x,
-			None => panic!("{}: unexpected empty line", fileinfo),
-		};
-		let f2 = match iter.next() {
-			Some(x) => x,
-			None => panic!("{}: missing character information for {}", fileinfo, f1),
-		};
+	let chars = lines.map(move |(line, n)| {
+		//----[ Helpers ]-----------------------------------------------------//
 
-		let (code, char, desc) = if let Some(desc) = iter.next() {
-			if let Some(value) = iter.next() {
-				panic!("{}: unexpected extra field `{}` at {}", fileinfo, value, f1);
-			}
-			(f1, f2, desc)
-		} else {
-			(f1, "", f2)
-		};
+		// Line and file information.
+		let info = || format!("{}:{}: ", fileinfo, n);
 
-		if !RE_CHAR.is_match(&code) {
-			panic!("{}: invalid character code `{}`", fileinfo, code);
+		// Validates and returns the next split field.
+		enum Next {
+			Field(&'static str),
+			End,
 		}
 
-		let code_chr: u32 = u32::from_str_radix(&code[2..], 16).unwrap();
-		let code_chr = match char::from_u32(code_chr) {
-			Some(x) => x,
-			None => panic!("{}: invalid codepoint `{}`", fileinfo, char),
+		// Split the line into tabs and returns an iterator function.
+		fn split_tabs<'a, F: Fn() -> String>(
+			line: &'a str,
+			info: F,
+		) -> impl FnMut(Next) -> &'a str {
+			let mut iter = line.trim().split('\t');
+			let next = move |kind: Next| match kind {
+				Next::Field(name) => match iter.next() {
+					Some(x) => x,
+					None => panic!("{}: expected {}", info(), name),
+				},
+				Next::End => {
+					if let Some(_) = iter.next() {
+						panic!("{}: unexpected extra field", info());
+					} else {
+						""
+					}
+				}
+			};
+
+			next
+		}
+
+		// Parses a UNICODE character code into a char.
+		let parse_char = |input: &str| {
+			if !RE_CHAR.is_match(input) {
+				panic!("{}: invalid character code `{}`", info(), input);
+			}
+
+			let char: u32 = u32::from_str_radix(&input[2..], 16).unwrap();
+			let char = match char::from_u32(char) {
+				Some(x) => x,
+				None => panic!("{}: invalid codepoint `{}`", info(), input),
+			};
+
+			char
 		};
 
-		if char.len() > 0 {
-			let chars = char.chars().collect::<Vec<char>>();
-			if chars.len() > 1 {
-				panic!("{}: invalid character `{}` at `{}`", fileinfo, char, code);
-			} else if chars[0] != code_chr {
+		//----[ Line parsing ]------------------------------------------------//
+
+		let line = line.trim();
+		let info = if line.len() == 0 {
+			None
+		} else if line.starts_with(":=") {
+			let line = line[2..].trim();
+			let flags = match line.parse::<kana::Flags>() {
+				Ok(flags) => flags,
+				Err(err) => panic!("{}: {}", info(), err),
+			};
+			Some(TestCharInfo::Flags(flags))
+		} else if line.starts_with(">") {
+			//----
+			// Parse a literal list of characters
+			//----
+			let line = line[1..].trim();
+			Some(TestCharInfo::List(
+				line.chars().filter(|&x| x != ' ' && x != '\t').collect(),
+			))
+		} else if line.starts_with("~") {
+			//----
+			// Parse a tab-separated character range
+			//----
+			let mut read = split_tabs(&line[1..], info);
+			let txt_start = read(Next::Field("range start"));
+			let txt_end = read(Next::Field("range end"));
+			let description = read(Next::Field("range description"));
+			read(Next::End);
+
+			let start = parse_char(txt_start);
+			let end = parse_char(txt_end);
+			if end <= start {
+				panic!("{}: invalid range ({}..{})", info(), txt_start, txt_end);
+			}
+
+			let description = description.to_string();
+			Some(TestCharInfo::Range {
+				start,
+				end,
+				description,
+			})
+		} else {
+			//----
+			// Parse a tab-separated single character
+			//----
+			let mut read = split_tabs(line, info);
+			let txt_code = read(Next::Field("character code"));
+			let txt_char = read(Next::Field("character literal")).trim();
+			let description = read(Next::Field("character description"));
+			read(Next::End);
+
+			// parse the character code, this is the one we really care about
+			let char = parse_char(txt_code);
+
+			// validate that the literal is at most a single char
+			if txt_char.chars().count() > 1 {
 				panic!(
-					"{}: character does not match code (`{}` != `{}`)",
-					fileinfo, code, char
+					"{}: invalid character literal for {} (`{}`)",
+					info(),
+					txt_code,
+					txt_char
 				);
 			}
-		}
 
-		TestChar {
-			char: code_chr,
-			code: code.to_string(),
-			description: desc.to_string(),
+			// if there is a literal, validate that it matches the code (the
+			// literal is informative only)
+			if let Some(char_literal) = txt_char.chars().next() {
+				if char_literal != char {
+					panic!(
+						"{}: character literal does not match code ({} != `{}`)",
+						info(),
+						txt_code,
+						txt_char
+					);
+				}
+			}
+
+			let code = txt_code.to_string();
+			let description = description.to_string();
+			Some(TestCharInfo::Single {
+				char,
+				code,
+				description,
+			})
+		};
+
+		match info {
+			Some(info) => Some(TestChar { line: n, info }),
+			None => None,
 		}
 	});
+	let chars = chars.flatten();
 	Ok(chars)
 }
--- tests/self_check.rs
+++ tests/self_check.rs
@@ -1,5 +1,7 @@
 //! Self-check for the integration tests.
 
+use common::{TestChar, TestCharInfo};
+
 mod common;
 
 /// Test that we can call the library functions.
@@ -14,6 +16,7 @@ fn test_works() {
 fn test_can_read_testdata() {
 	let lines: Vec<String> = common::read_lines("./testdata/self_check.in")
 		.unwrap()
+		.map(|(x, _)| x)
 		.collect();
 	assert_eq!(lines, vec!["line 1", "line 2"]);
 }
@@ -21,19 +24,20 @@ fn test_can_read_testdata() {
 /// Test that we can read and parse the `testdata` character tables.
 #[test]
 fn test_can_read_chardata() {
-	let chars: Vec<_> = common::read_chars("./testdata/chars/hiragana.in")
+	let chars: Vec<_> = common::read_chars("./testdata/chars/flags.in")
 		.unwrap()
 		.collect();
 	chars
 		.iter()
-		.find(|it| it.char == 'あ' && it.description.to_lowercase() == "hiragana letter a")
+		.find(|TestChar { info, .. }| {
+			if let TestCharInfo::Single {
+				char, description, ..
+			} = info
+			{
+				*char == 'あ' && description.to_lowercase() == "hiragana letter a"
+			} else {
+				false
+			}
+		})
 		.expect("should have 'あ'");
-
-	let chars: Vec<_> = common::read_chars("./testdata/chars/spaces.in")
-		.unwrap()
-		.collect();
-	chars
-		.iter()
-		.find(|it| it.char == ' ' && it.description.to_lowercase() == "space (sp)")
-		.expect("should have the space character");
 }
